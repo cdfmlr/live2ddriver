03d8ff50bcb7e1c2b216e741f5c65188678481fb
CDFMLR <cdfmlr@163.com>
2023-07-08 21:09:53 +0800

    feat: Live2DRequest supports the speak command
    配合 live2dview v0.1.0

--- live2ddriver/model.go
+++ live2ddriver/model.go
@@ -2,9 +2,20 @@ package live2ddriver
 
 // Live2DRequest is the message format for Live2DView controlling (communication).
 type Live2DRequest struct {
-	Model      string `json:"model,omitempty"`      // model src
-	Motion     string `json:"motion,omitempty"`     // motion group
+	Model      string    `json:"model,omitempty"`      // model src
+	Motion     string    `json:"motion,omitempty"`     // motion group
+	Expression string    `json:"expression,omitempty"` // expression id (name or index)
+	Speak      *Speaking `json:"speak,omitempty"`      // speak audio (lip sync)
+}
+
+// Speaking is the message format for Live2DView speaking (lip sync).
+type Speaking struct {
+	Audio  string  `json:"audio,omitempty"`  // audio src: url to audio file (wav or mp3) or base64 encoded data (data:audio/wav;base64,xxxx)
+	Text   string  `json:"text,omitempty"`   // text
+	Volume float32 `json:"volume,omitempty"` // volume
+
 	Expression string `json:"expression,omitempty"` // expression id (name or index)
+	Motion     string `json:"motion,omitempty"`     // motion group
 }
 
 // chan buffer size
